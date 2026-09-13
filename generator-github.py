129ac8d2be1b4281bf825f38ef982540b8a82368
FlyingDashie <102208072+FlyingDashie@users.noreply.github.com>
2026-09-13 18:23:01 +0800

    Add files via upload

--- generator-github.py
+++ generator-github.py
@@ -3800,7 +3800,7 @@ def find_or_install_mihomo() -> Path:
     archive = _toolkit_fetch_package(
         "https://github.com/MetaCubeX/mihomo",
         install_dir,
-        prefer=[os_token, *arch_tokens, "mihomo", r"v\d+\."],
+        prefer=[os_token, *arch_tokens, "mihomo", r"v\d+\.", "gz", "zip", "7z"],
         require_sha256=True,
     )
     if archive is None:
